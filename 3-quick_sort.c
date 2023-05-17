227a953ca3a89037c6fdd5cd886030cccf26f44d
KrasniKot <6187@holbertonstudents.com>
2023-05-17 15:46:08 -0700

    task 3 in progress

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,29 @@
 #include "sort.h"
 
+int partitioner(int *array, int start, int end)
+{
+	int pvt = array[end];
+	int i = start - 1, j, temp;
+
+	for (j = start; j <= end - 1; j++)
+		if (array[j] < pvt)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+
+}
+
+void sorter(int *array, int start, int end)
+{
+	int pvt = partitioner(array, start, end);
+
+	if (array[0] <> array[end])
+		return;
+	sorter(array, start, pvt - 1);
+	sorter(array, pvt + 1, end);
+}
+
 /**
  * quick_sort - sorts an array of ints using quick sort algorithm.
  * @array: array to be sorted.
@@ -7,4 +31,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
+	int pvt;
+
+	sorter(array, array[0], array[size]);
 }
