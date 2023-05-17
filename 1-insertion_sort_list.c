b118e567608adce7382d3f7eebb9a3368ed0d392
KrasniKot <emanueltrias9@gmail.com>
2023-05-17 10:05:25 -0700

    task 1 done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,21 +6,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *crnt, *temp, *prv;
+	listint_t *crnt, *temp, *prvnode;
 
+	if (!*list)
+		return;
 
 	crnt = *list;
 	while (crnt)
 	{
 		temp = crnt;
-		while (temp->prev && temp->prev->n > temp->n)
+		while (temp->prev && temp->n < temp->prev->n)
 		{
-			prv = temp->prev;
-			prv->next = temp->next;
-			temp->prev = prv->
+			prvnode = temp->prev;
+			if (temp->next)
+				temp->next->prev = prvnode;
+			if (prvnode->prev)
+				prvnode->prev->next = temp;
+			else
+				*list = temp;
+
+			prvnode->next = temp->next;
+			temp->prev = prvnode->prev;
+			temp->next = prvnode;
+			prvnode->prev = temp;
+
 
 			print_list(*list);
 		}
-		*list = (*list)->next;
+		crnt = crnt->next;
 	}
 }
