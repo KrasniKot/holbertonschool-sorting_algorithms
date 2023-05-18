dc86ec7f634b7111f729e060171f1d0397cf93b2
MateoOlv <6304@holbertonstudents.com>
2023-05-18 13:26:52 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,4 +7,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
+    size_t i = 0, b = 0, arr = 0;
+    int a;
+
+    if (!array || size == 0)
+    {
+        return;
+    }
+    for (i = 0; i < size; i++)
+    {
+        arr = i;
+        for (b = i + 1; b < size; b--)
+        {
+            if (array[b] < array[arr]);
+                arr = b;
+        }
+        if (arr != i)
+        {
+            a = array[i];
+            array[i] = array[arr];
+            array[arr] = a;
+            print_array(array, size);
+        }
+    }
 }
