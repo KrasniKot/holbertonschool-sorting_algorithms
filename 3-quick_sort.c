20e153002923541dafaa4d441cd2ab95ee9dd54e
KrasniKot <6187@holbertonstudents.com>
2023-05-17 19:17:25 -0700

    task 3 redo

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,69 @@
 #include "sort.h"
 
 /**
- * swap - swaps two elements.
- * @x: first element.
- * @y: second element.
- */
-void swap(int *x, int *y)
-{
-	int tmp;
-
-	tmp = *y;
-	*y = *x;
-	*x = tmp;
-}
-
-/**
- * partitioner - returns the right location for the pivot.
- * @array: array.
+ * partitioner - returns a pivot in order to sort a sub array.
+ * @array: array to sort.
+ * @start: start.
+ * @end: end.
  * @size: size of the array.
- * Return: the location of the pivot.
+ * Reurn: the pivot.
  */
-int partitioner(int *array, size_t size)
+int partitioner(int *array, int start, int end, size_t size)
 {
-	int pvt = array[size];
-	size_t i = -1, j;
+	int i = start - 1, j, temp;
 
-	for (j = 0; j < size; j++)
-		if (array[j] <= pvt)
+	for (j = start; j <= end - 1; j++)
+	{
+		if (array[j] < array[end])
 		{
 			i++;
-			if (i != j)
+			if (i < j)
 			{
-				swap(&array[i], &array[j]);
-				print_array(array, size + 1);
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
 			}
 		}
-	i++;
-	if (i != size)
-	{
-		swap(&array[i], &array[size]);
-		print_array(array, size + 1);
 	}
 
+	i++;
+	if (array[i] > array[end])
+	{
+		temp = array[i];
+		array[i] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
 	return (i);
 
 }
 
 /**
- * quick_sort - sorts an array of ints using quick sort algorithm.
+ * sorter - sorts an array of ints using quick sort algorithm.
+ * @array: array to sort.
+ * @start: start.
+ * @end: end.
+ * @size: size.
+ */
+void sorter(int *array, int start, int end, size_t size)
+{
+	int pvt;
+
+	if (start < end)
+	{
+		pvt = partitioner(array, start, end, size);
+		sorter(array, start, pvt - 1, size);
+		sorter(array, pvt + 1, end, size);
+	}
+}
+
+/**
+ * quick_sort - calls to sorter.
  * @array: array to be sorted.
  * @size: size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t pvt;
-
-	if (!array || size < 2)
-		return;
-
-	pvt = partitioner(array, size - 1);
-
-	quick_sort(array, pvt);
-	quick_sort(array + pvt, size - pvt);
-
+	sorter(array, 0, size - 1, size);
 }
